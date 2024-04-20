74bb3e59d7600ad4556361ce9d714db39a921a18
codeAmbivert <titilopechisom20@gmail.com>
2024-04-20 17:28:18 +0100

    id page

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -1,11 +1,24 @@
 "use client";
-import { FaStar } from "react-icons/fa";
+import { FaCheck, FaStar } from "react-icons/fa";
+import { FiMinus, FiPlus } from "react-icons/fi";
 import { LiaBookReaderSolid } from "react-icons/lia";
 import Layout from "../../components/layout/UserLayout";
 import { useState } from "react";
 import Viewer from "react-viewer";
+import {
+  DatePicker,
+  DateTimePicker,
+  LocalizationProvider,
+} from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
 const RoomPage = () => {
+  const [occupants, setOccupants] = useState(1);
+  const [openCheckIn, setOpenCheckIn] = useState(false);
+  const [openCheckOut, setOpenCheckOut] = useState(false);
+  const [checkInDate, setcheckInDate] = useState(dayjs());
+  const [checkOutDate, setcheckOutDate] = useState(dayjs());
   const [visible, setVisible] = useState(false);
   const [activeIndex, setActiveIndex] = useState(0);
   const images = [
@@ -15,6 +28,7 @@ const RoomPage = () => {
     "https://a0.muscache.com/im/pictures/miso/Hosting-53627561/original/cc19cf5f-d04f-4b61-99b0-53b77aca7ba6.jpeg?im_w=720",
   ];
 
+  console.log({ checkInDate });
   return (
     <Layout>
       <div className="max-w-7xl w-full mx-auto p-5">
@@ -58,9 +72,9 @@ const RoomPage = () => {
             ))}
         </div>
 
-        <div className="mt-10 flex">
+        <div className="mt-10 flex gap-10">
           <div>
-            <p className="text-3xl font-semibold">Room overview</p>
+            <p className="text-3xl font-semibold mb-5">Room overview</p>
             <p>
               Step into comfort and elegance with our luxurious hotel rooms.
               Designed for relaxation, each room features modern amenities,
@@ -68,8 +82,121 @@ const RoomPage = () => {
               business or leisure, our rooms provide the perfect retreat for a
               rejuvenating stay.
             </p>
+            <div className="mt-5 pt-5">
+              <p className="text-3xl font-semibold mb-5">
+                What&apos;s included
+              </p>
+              <div className="flex flex-col gap-3">
+                <div className="flex items-center gap-2">
+                  <div className="rounded-full p-1 bg-green-100 text-green-500 h-fit">
+                    <FaCheck size={12} />
+                  </div>
+                  <p>Beverages, drinking water, morning tea and buffet lunch</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <div className="rounded-full p-1 bg-green-100 text-green-500 h-fit">
+                    <FaCheck size={12} />
+                  </div>
+                  <p>Beverages, drinking water, morning tea and buffet lunch</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <div className="rounded-full p-1 bg-green-100 text-green-500 h-fit">
+                    <FaCheck size={12} />
+                  </div>
+                  <p>Beverages, drinking water, morning tea and buffet lunch</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <div className="rounded-full p-1 bg-green-100 text-green-500 h-fit">
+                    <FaCheck size={12} />
+                  </div>
+                  <p>Beverages, drinking water, morning tea and buffet lunch</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <div className="rounded-full p-1 bg-green-100 text-green-500 h-fit">
+                    <FaCheck size={12} />
+                  </div>
+                  <p>Beverages, drinking water, morning tea and buffet lunch</p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div></div>
+          <div className="border rounded-xl p-3 w-[45rem] h-fit">
+            <p className="whitespace-nowrap text-lg font-semibold">
+              &#8358;82,000
+            </p>
+
+            <div className="text-swGray900 top-24 bg-white w-full rounded-md z-20 mt-5">
+              <div className="flex flex-col gap-5 font-medium">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <div className="rooms flex flex-col gap-2 w-full">
+                    <p>Check-in Date</p>
+                    <div className="flex relative w-full">
+                      <p className="absolute top-1/2 -translate-y-1/2 ml-4">
+                        {checkInDate.format("DD-MM-YY")}
+                      </p>
+                      <DatePicker
+                        // label="Departure Date"
+                        // defaultValue={dayjs()}
+                        value={checkInDate}
+                        // onClose={() => setDateOpen(null)}
+                        onChange={(value) => setcheckInDate(value)}
+                        // renderInput={(params) => <TextField {...params} />}
+                        className="w-full"
+                      />
+                    </div>
+                    <p>Check-out Date</p>
+                    <div className="flex relative w-full">
+                      <p className="absolute top-1/2 -translate-y-1/2 ml-4">
+                        {checkOutDate.format("DD-MM-YY")}
+                      </p>
+                      <DatePicker
+                        // label="Arrival Date"
+                        // defaultValue={dayjs()}
+                        value={checkOutDate}
+                        onChange={(value) => setcheckOutDate(value)}
+                        // onClose={() => setDateOpen(null)}
+                        // renderInput={(params) => <TextField {...params} />}
+                        className="w-full"
+                      />
+                    </div>
+                  </div>
+                </LocalizationProvider>
+                <div className="flex justify-between items-center">
+                  <p className="">Occupants</p>
+                  <div className="rounded-md overflow-hidden flex">
+                    <p
+                      className="p-2 cursor-pointer rounded-full border"
+                      onClick={() =>
+                        setOccupants(occupants > 1 ? occupants - 1 : occupants)
+                      }
+                    >
+                      <FiMinus size={20} />
+                    </p>
+                    <p className="h-10 w-14 flex justify-center items-center">
+                      {occupants}
+                    </p>
+                    <p
+                      className="p-2 cursor-pointer rounded-full border"
+                      onClick={() => setOccupants(occupants + 1)}
+                    >
+                      <FiPlus size={20} />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-10 pt-5 border-t">
+              <div className="flex justify-between items-center text-lg font-medium">
+                <p>Total:</p>
+                <p className="whitespace-nowrap">&#8358;82,000</p>
+              </div>
+
+              <div className="bg-[#B89010] hover:bg-[#C8A008] cursor-pointer mt-2 font-medium p-5 rounded-md text-center text-white">
+                Book Now
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <Viewer
