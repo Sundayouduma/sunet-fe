f09b8656ec543a00cb104accb491ce1f73db43c9
codeAmbivert <titilopechisom20@gmail.com>
2024-05-28 11:43:09 +0100

    refactor: Remove unnecessary attributes from Google Maps iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -422,10 +422,7 @@ export default function Home() {
               <iframe
                 width="100%"
                 height="400"
-                frameborder="0"
                 scrolling="no"
-                marginheight="0"
-                marginwidth="0"
                 src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=en&amp;q=No%201,%20lukulu%20Street%20wuse%20zone%203,%20Abuja+(Juwsheyaj%20Hotel)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
               >
                 <a href="https://www.gps.ie/">gps trackers</a>
